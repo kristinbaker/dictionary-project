86de702cb74c0385ab12ddd4cfa06ad07cafff89
Kristin Baker <87988811+kristinbaker@users.noreply.github.com>
2021-10-22 08:52:02 -0500

    added form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Hello
+       Dictionary 
       </header>
+      <div className="App-body">What word do you want to look up? </div>
+      <Dictionary />
+      <footer className="App-footer"><small>
+        This page was coded by Kristin Baker, is open-sourced on Github, and is hosted by Netlify
+        </small>
+      </footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Dictionary.css"
+
+export default function Dictionary(){
+    return (
+        <div className="Dictionary">
+        <form> <input className="Search-Form" type="search" placeholder="Search for a word"/> </form>
+        </div>
+       
+    ); 
+}
